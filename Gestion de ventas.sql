bad6627b11071d0bc80001250d987e5a945fcf44
harold_20_06 <harold_20_06@hotmail.com>
2023-01-10 23:06:48 -0400

    add consultas sobre tablas DB ventas

--- Gestion de ventas.sql
+++ Gestion de ventas.sql
@@ -80,3 +80,76 @@ SELECT  * FROM cliente
 select * from comercial
 select * from pedido
 
+--Consultas sobre una tabla
+
+--1. Devuelve un listado con todos los pedidos que se han realizado. Los pedidos deben estar ordenados por la fecha de realización, mostrando en primer lugar los pedidos más recientes.
+select * from pedido order by fecha
+--2. Devuelve todos los datos de los dos pedidos de mayor valor.
+select * from pedido order by total desc limit 2
+--3. Devuelve un listado con los identificadores de los clientes que han realizado algún pedido. Tenga en cuenta que no debe mostrar identificadores que estén repetidos.
+select distinct id_cliente from pedido 
+--4. Devuelve un listado de todos los pedidos que se realizaron durante el año 2017, cuya cantidad total sea superior a 500€.
+select * from pedido where EXTRACT(YEAR FROM fecha) = '2017' and total > 500
+--5. Devuelve un listado con el nombre y los apellidos de los comerciales que tienen una comisión entre 0.05 y 0.11.
+select * from comercial where comisión between 0.05 and 0.11
+--6. Devuelve el valor de la comisión de mayor valor que existe en la tabla comercial.
+select max(comisión) from comercial
+--7. Devuelve el identificador, nombre y primer apellido de aquellos clientes cuyo segundo apellido no es NULL. El listado deberá estar ordenado alfabéticamente por apellidos y nombre.
+select id, nombre, apellido1 from cliente where apellido2 is not null order by apellido1, apellido2, nombre
+--8. Devuelve un listado de los nombres de los clientes que empiezan por A y terminan por n y también los nombres que empiezan por P. El listado deberá estar ordenado alfabéticamente.
+select nombre from cliente where nombre like 'A%n' or nombre like 'P%' order by nombre asc
+--9. Devuelve un listado de los nombres de los clientes que no empiezan por A. El listado deberá estar ordenado alfabéticamente.
+select nombre from cliente where nombre not like 'A%' order by nombre asc
+--10. Devuelve un listado con los nombres de los comerciales que terminan en o. Tenga en cuenta que se deberán eliminar los nombres repetidos.
+select distinct nombre from comercial where nombre like '%o' 
+
+--Consultas multitabla (Composición interna)
+
+--Resuelva todas las consultas utilizando la sintaxis de SQL1 y SQL2.
+
+--1. Devuelve un listado con el identificador, nombre y los apellidos de todos los clientes que han realizado algún pedido. El listado debe estar ordenado alfabéticamente y se deben eliminar los elementos repetidos.
+select distinct c.id,c.nombre,c.apellido1, c.apellido2 from pedido p inner join cliente c on p.id_cliente = c.id 
+order by c.id 
+--2. Devuelve un listado que muestre todos los pedidos que ha realizado cada cliente. El resultado debe mostrar todos los datos de los pedidos y del cliente. El listado debe mostrar los datos de los clientes ordenados alfabéticamente.
+select c.*,p.* from pedido p inner join cliente c on p.id_cliente = c.id  
+order by c.id, p.id
+--3. Devuelve un listado que muestre todos los pedidos en los que ha participado un comercial. El resultado debe mostrar todos los datos de los pedidos y de los comerciales. El listado debe mostrar los datos de los comerciales ordenados alfabéticamente.
+select * from pedido p inner join comercial c on p.id_comercial = c.id order by c.id 
+--4. Devuelve un listado que muestre todos los clientes, con todos los pedidos que han realizado y con los datos de los comerciales asociados a cada pedido.
+select * from cliente c inner join pedido p on c.id = p.id_cliente 
+                        inner join comercial co on p.id_comercial = co.id 			
+--5. Devuelve un listado de todos los clientes que realizaron un pedido durante el año 2017, cuya cantidad esté entre 300 € y 1000 €.
+select * from cliente c inner join pedido p on c.id = p.id_cliente 
+where extract(year from fecha) = 2017
+and p.total between 300 and 1000  
+--6. Devuelve el nombre y los apellidos de todos los comerciales que ha participado en algún pedido realizado por María Santana Moreno.
+select distinct co.nombre, co.apellido1, co.apellido2  from cliente c inner join pedido p on c.id = p.id_cliente 
+                        inner join comercial co on p.id_comercial = co.id 			
+where c.nombre = 'María' and c.apellido1 = 'Santana' and c.apellido2 = 'Moreno'
+--7. Devuelve el nombre de todos los clientes que han realizado algún pedido con el comercial Daniel Sáez Vega.
+select distinct c.nombre,c.apellido1, c.apellido2 from cliente c inner join pedido p on c.id = p.id_cliente 
+                        inner join comercial co on p.id_comercial = co.id 			
+where co.nombre = 'Daniel' and co.apellido1 = 'Sáez' and co.apellido2 = 'Vega'
+
+--Consultas multitabla (Composición externa)
+
+--Resuelva todas las consultas utilizando las cláusulas LEFT JOIN y RIGHT JOIN.
+
+--1. Devuelve un listado con todos los clientes junto con los datos de los pedidos que han realizado. Este listado también debe incluir los clientes que no han realizado ningún pedido. El listado debe estar ordenado alfabéticamente por el primer apellido, segundo apellido y nombre de los clientes.
+select * from cliente c left join pedido p on c.id = p.id_cliente 
+order by c.apellido1, c.apellido2, c.nombre
+--2. Devuelve un listado con todos los comerciales junto con los datos de los pedidos que han realizado. Este listado también debe incluir los comerciales que no han realizado ningún pedido. El listado debe estar ordenado alfabéticamente por el primer apellido, segundo apellido y nombre de los comerciales.
+select * from comercial co left join pedido p on co.id = p.id_comercial 
+order by co.apellido1, co.apellido2, co.nombre
+--3. Devuelve un listado que solamente muestre los clientes que no han realizado ningún pedido.
+select * from cliente c left join pedido p on c.id = p.id_cliente 
+where p.id is null
+--4. Devuelve un listado que solamente muestre los comerciales que no han realizado ningún pedido.
+select * from comercial co left join pedido p on co.id = p.id_comercial 
+where p.id is null
+--5. Devuelve un listado con los clientes que no han realizado ningún pedido y de los comerciales que no han participado en ningún pedido. Ordene el listado alfabéticamente por los apellidos y el nombre. En en listado deberá diferenciar de algún modo los clientes y los comerciales.
+select c.nombre, c.apellido1,c.apellido2, 'cliente' as tipo  from cliente c left join pedido p on c.id = p.id_cliente 
+where p.id is null
+union all
+select co.nombre, co.apellido1,co.apellido2, 'comercial' from comercial co left join pedido p on co.id = p.id_comercial 
+where p.id is null
