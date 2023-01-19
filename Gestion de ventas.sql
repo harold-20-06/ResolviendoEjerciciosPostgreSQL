4bcf10f6846cc38a32d9868a85190d21db493d2f
harold_20_06 <harold_20_06@hotmail.com>
2023-01-18 20:34:12 -0400

    add consultas mutitablas DB ventas

--- Gestion de ventas.sql
+++ Gestion de ventas.sql
@@ -153,3 +153,110 @@ where p.id is null
 union all
 select co.nombre, co.apellido1,co.apellido2, 'comercial' from comercial co left join pedido p on co.id = p.id_comercial 
 where p.id is null
+
+--Consultas resumen
+
+--1. Calcula la cantidad total que suman todos los pedidos que aparecen en la tabla pedido.
+select sum(total) from pedido
+--2. Calcula la cantidad media de todos los pedidos que aparecen en la tabla pedido.
+select avg(total) from pedido
+--3. Calcula el número total de comerciales distintos que aparecen en la tabla pedido.
+select count(distinct id_comercial) from pedido
+--4. Calcula el número total de clientes que aparecen en la tabla cliente.
+select count (id) from cliente
+--5. Calcula cuál es la mayor cantidad que aparece en la tabla pedido.
+select max(total) from pedido
+--6. Calcula cuál es la menor cantidad que aparece en la tabla pedido.
+select min(total) from pedido
+--7. Calcula cuál es el valor máximo de categoría para cada una de las ciudades que aparece en la tabla cliente.
+select ciudad, max(categoría) from cliente
+group by ciudad
+order by ciudad
+--8. Calcula cuál es el máximo valor de los pedidos realizados durante el mismo día para cada uno de los clientes. 
+--Es decir, el mismo cliente puede haber realizado varios pedidos de diferentes cantidades el mismo día. 
+--Se pide que se calcule cuál es el pedido de máximo valor para cada uno de los días en los que un cliente ha realizado un pedido. 
+--Muestra el identificador del cliente, nombre, apellidos, la fecha y el valor de la cantidad.
+select c.id, c. nombre, c.apellido1, c.apellido2, p.fecha, max(p.total) from cliente c inner join pedido p on p.id_cliente = c.id
+group by c.id,fecha
+order by c.id, fecha 
+--9. Calcula cuál es el máximo valor de los pedidos realizados durante el mismo día para cada uno de los clientes, 
+--teniendo en cuenta que sólo queremos mostrar aquellos pedidos que superen la cantidad de 2000 €.
+select c.id, c. nombre, c.apellido1, c.apellido2, p.fecha, max(p.total) from cliente c inner join pedido p on p.id_cliente = c.id
+where p.total > 2000
+group by c.id,fecha
+order by c.id, fecha 
+--10. Calcula el máximo valor de los pedidos realizados para cada uno de los comerciales durante la fecha 2016-08-17. 
+--Muestra el identificador del comercial, nombre, apellidos y total.
+select co.id, co.nombre, co.apellido1,co.apellido2, max(p.total), p.fecha  from comercial co inner join pedido p on co.id = p.id_comercial
+where date(p.fecha) = '2016-08-17'
+group by co.id,fecha
+order by p.fecha
+--11. Devuelve un listado con el identificador de cliente, nombre y apellidos y 
+--el número total de pedidos que ha realizado cada uno de clientes. 
+--Tenga en cuenta que pueden existir clientes que no han realizado ningún pedido. 
+--Estos clientes también deben aparecer en el listado indicando que el número de pedidos realizados es 0.
+select c.id, c. nombre, c.apellido1, c.apellido2, count(p.id_cliente) from cliente c left join pedido p on p.id_cliente = c.id
+group by c.id
+order by c.id 
+--12. Devuelve un listado con el identificador de cliente, nombre y apellidos y el número total de pedidos que 
+--ha realizado cada uno de clientes durante el año 2017.
+select c.id, c. nombre, c.apellido1, c.apellido2, count(p.id_cliente) from cliente c inner join pedido p on p.id_cliente = c.id
+where extract(year from fecha) = 2017
+group by c.id
+order by c.id 
+--13. Devuelve un listado que muestre el identificador de cliente, nombre, primer apellido y 
+--el valor de la máxima cantidad del pedido realizado por cada uno de los clientes. 
+--El resultado debe mostrar aquellos clientes que no han realizado ningún pedido indicando que 
+--la máxima cantidad de sus pedidos realizados es 0. Puede hacer uso de la función IFNULL.
+select c.id, c. nombre, c.apellido1, c.apellido2, max(COALESCE(p.total,0))  from cliente c left join pedido p on p.id_cliente = c.id
+group by c.id
+order by c.id
+--14. Devuelve cuál ha sido el pedido de máximo valor que se ha realizado cada año.
+select max(total), extract (year from fecha) from pedido
+group by extract (year from fecha)
+order by extract (year from fecha) 
+--15. Devuelve el número total de pedidos que se han realizado cada año.
+select count(id), extract (year from fecha) from pedido
+group by extract (year from fecha)
+--Subconsultas
+--Con operadores básicos de comparación
+
+--1. Devuelve un listado con todos los pedidos que ha realizado Adela Salas Díaz. (Sin utilizar INNER JOIN).
+Select * from pedido 
+where id_cliente = (select id from cliente where nombre = 'Adela' and apellido1 ='Salas' and apellido2 = 'Díaz');
+--2. Devuelve el número de pedidos en los que ha participado el comercial Daniel Sáez Vega. (Sin utilizar INNER JOIN)
+select count(*) from pedido
+where id_comercial = (select id from comercial where nombre = 'Daniel' and apellido1 ='Sáez' and apellido2 = 'Vega');
+--3. Devuelve los datos del cliente que realizó el pedido más caro en el año 2019. (Sin utilizar INNER JOIN)
+select * from cliente
+where id = (select id_cliente from pedido
+where total = (select max(total) from pedido where extract(year from fecha) = 2019))
+--4. Devuelve la fecha y la cantidad del pedido de menor valor realizado por el cliente Pepe Ruiz Santana.
+Select fecha, total from cliente inner join pedido on cliente.id = pedido.id_cliente 
+where cliente.nombre = 'Pepe' and apellido1 = 'Ruiz' and apellido2 = 'Santana' and pedido.total = (select MIN(total) from cliente inner join pedido on cliente.id = pedido.id_cliente 
+where cliente.nombre = 'Pepe' and apellido1= 'Ruiz' and apellido2 = 'Santana');
+--5. Devuelve un listado con los datos de los clientes y los pedidos, de todos los clientes que han 
+--realizado un pedido durante el año 2017 con un valor mayor o igual al valor medio de los pedidos realizados durante ese mismo año.
+Select * from cliente inner join pedido on cliente.id = pedido.id_cliente 
+where year(pedido.fecha) =2017 and pedido.total >= (select avg(total) from pedido where year(fecha)=2017);
+
+--Subconsultas con ALL y ANY
+
+--6. Devuelve el pedido más caro que existe en la tabla pedido si hacer uso de MAX, ORDER BY ni LIMIT.
+Select * from pedido where total >= all(select total from pedido);
+--7. Devuelve un listado de los clientes que no han realizado ningún pedido. (Utilizando ANY o ALL).
+Select * from cliente where id!= all(select id_cliente from pedido);
+--8. Devuelve un listado de los comerciales que no han realizado ningún pedido. (Utilizando ANY o ALL).
+Select * from comercial where id!= all(select id_comercial from pedido)
+--Subconsultas con IN y NOT IN
+
+--9. Devuelve un listado de los clientes que no han realizado ningún pedido. (Utilizando IN o NOT IN).
+Select * from cliente where id not in (select id_cliente from pedido)
+--10. Devuelve un listado de los comerciales que no han realizado ningún pedido. (Utilizando IN o NOT IN).
+Select * from comercial where id not in (select id_comercial from pedido)
+--Subconsultas con EXISTS y NOT EXISTS
+
+--11. Devuelve un listado de los clientes que no han realizado ningún pedido. (Utilizando EXISTS o NOT EXISTS).
+Select * from cliente where not exists (select id_cliente from pedido where cliente.id =pedido.id_cliente);
+--12. Devuelve un listado de los comerciales que no han realizado ningún pedido. (Utilizando EXISTS o NOT EXISTS).
+Select * from comercial where not exists (select id_comercial from pedido where comercial.id =pedido.id_comercial)
